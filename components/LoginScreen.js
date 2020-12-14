4b76c62c11113d05d459ad8ceefe96493da5c08c
Yason Khaburzaniya <yason.khabur@yahoo.com>
2020-12-13 22:07:40 -0800

    some UI sugar

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -3,41 +3,47 @@
 import React from 'react';
 
 import {
-  TextInput, View, Button, StyleSheet,
+    TextInput, View, Button, StyleSheet, KeyboardAvoidingView,
 } from 'react-native';
 
-export function LoginScreen(props) {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
+export function LoginScreen ( props ) {
+    const [email, setEmail] = React.useState( '' );
+    const [password, setPassword] = React.useState( '' );
 
 
-  return (
-    <View style={styles.container}>
-      <View style={{ margin: 10 }} >
-        <TextInput
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-        />
-        <TextInput
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
-        <Button title="Sign in" onPress={() => {
-          props.onLogin(email, password)}
-        } />
-      </View>
-    </View>
-  );
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.OS == "ios" ? "padding" : "height"}
+            style={styles.container}
+        >
+            <View style={{margin: 10}}>
+                <TextInput
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={setEmail}
+                />
+                <TextInput
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                />
+                <Button title="Sign in" onPress={() => {
+                    props.onLogin( email, password )
+                }
+                }/>
+            </View>
+        </KeyboardAvoidingView>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+const styles = StyleSheet.create( {
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+} )
