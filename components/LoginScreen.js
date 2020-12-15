f3bc242b7abd36bf7ac2f2b6216f0810c8305bd9
Yason Khaburzaniya <yason.khabur@yahoo.com>
2020-12-14 18:37:17 -0800

    some cosmetic UX improvements to auth screen

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,6 +1,5 @@
 // @flow
-//import React and PureComponent to have the ability to have a shallow comparison of props and state
-import React from 'react';
+import React, {useRef} from 'react';
 
 import {
     TextInput, View, Button, StyleSheet, KeyboardAvoidingView,
@@ -10,6 +9,7 @@ export function LoginScreen ( props ) {
     const [email, setEmail] = React.useState( '' );
     const [password, setPassword] = React.useState( '' );
 
+    const ref_passowrd = useRef();
 
     return (
         <KeyboardAvoidingView
@@ -20,15 +20,20 @@ export function LoginScreen ( props ) {
                 <TextInput
                     keyboardType="email-address"
                     autoCapitalize="none"
+                    autoCorrect={false}
                     placeholder="Email"
                     value={email}
                     onChangeText={setEmail}
+                    onSubmitEditing={() => ref_passowrd.current.focus()}
+                    returnKeyType={"next"}
                 />
                 <TextInput
                     placeholder="Password"
                     value={password}
                     onChangeText={setPassword}
                     secureTextEntry
+                    ref={ref_passowrd}
+                    onSubmitEditing={() => props.onLogin( email, password )}
                 />
                 <Button title="Sign in" onPress={() => {
                     props.onLogin( email, password )
